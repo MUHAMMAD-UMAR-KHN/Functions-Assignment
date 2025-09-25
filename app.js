d69beaab32522a506e32a810b6549ba441978b18
MUHAMMAD-UMAR-KHN <uk1555464@gmail.com>
2025-09-25 12:00:27 +0500

    functions

---
+++ app.js
@@ -0,0 +1,254 @@
+//--------------------------chap 35 - 38---------------------
+
+//Q1
+
+function dateAndTime() {
+    var date = new Date();
+    console.log(date.toString());
+}
+DateAndTime();
+
+
+
+
+//Q2
+
+function fullName(firstName, lastName) {
+    var fullName = firstName + lastName;
+    console.log("Hello", firstName, lastName);
+}
+fullName(prompt("Enter fisrt name: "), prompt("Enter last name: "));
+
+
+
+
+
+//Q3
+
+function add(a, b) {
+    var sum = a + b;
+    console.log("The sum of numbers is", sum);
+}
+add(prompt("Enter the first number"), prompt("Enter the second number"));
+
+
+
+
+//Q4
+
+function calculator(num1, num2, operator) {
+    if (operator == "+") {
+        console.log("the Sum of the numbers is:", num1 + num2);
+    }
+    else if (operator == "-") {
+        console.log("the Difference of the numbers is:", num1 - num2);
+    }
+    else if (operator == "x") {
+        console.log("the Product of the numbers is:", num1 * num2);
+    }
+    else if (operator == "/") {
+        console.log("the Division of the numbers is:", num1 / num2);
+    }
+}
+calculator(+prompt("Enter the first number"), +prompt("Enter the second number"), prompt("Enter the operator: "));
+
+
+
+
+//Q5
+
+function square(number) {
+    var squares = number * number;
+    console.log("The squae of the number is", squares);
+}
+square(+prompt("Enter the number: "));
+
+
+
+
+//Q6
+
+function factorial(number) {
+    var factorial = 1;
+    for (let i = number; i >= 1; i--) {
+        factorial = factorial * i;
+    }
+    console.log(`Factorial of ${number} is ${factorial}`);
+}
+factorial(+prompt("Enter the number: "));
+
+
+
+
+//Q7
+
+function counting(startingNumber, endingNumber) {
+    var countingList = [];
+    for (let i = startingNumber; i <= endingNumber; i++) {
+        countingList.push(i);
+    }
+    console.log(`The Counting between ${startingNumber} and ${endingNumber} is ${countingList.toString()}`);
+}
+counting(+prompt("Enter the starting number: "), +prompt("Enter the ending number: "));
+
+
+
+
+
+//Q8
+
+var base = +prompt("Enter the Base: ");
+var perpendicular = +prompt("Enter the Perpendicular: ");
+function calculateHypotenuse(base, perpendicular) {
+    function calculateSquare(x) {
+        return x * x;
+    }
+    var hypotenuse = Math.sqrt(calculateSquare(base) + calculateSquare(perpendicular));
+    console.log(`The hypotenuse is ${hypotenuse}`);
+}
+
+calculateHypotenuse(base, perpendicular);
+
+
+
+
+
+//Q9
+
+var width = +prompt("Enter the width: ");
+var height = +prompt("Enter the height: ");
+function area(width, height) {
+    var area = width * height;
+    console.log("The area is", area);
+}
+area(width, height);
+
+
+
+
+
+//Q10
+
+var word = prompt("Enter the word: ");
+function palindromeChecker(word) {
+    word = word.toLowerCase();
+    var reversedWord = word.split('').reverse().join('');
+    if (word === reversedWord) {
+        console.log("The word is a palindrome");
+    } else {
+        console.log("The word is not a palindrome");
+    }
+}
+palindromeChecker(word);
+
+
+
+
+//Q11
+
+function upperCaseLetterConverter(statement) {
+    var sentence = statement.toLowerCase().split(" ");
+    for (let i = 0; i < sentence.length; i++) {
+        sentence[i] = sentence[i].charAt(0).toUpperCase() + sentence[i].slice(1);
+    }
+
+    var converted = sentence.join(" ");
+    console.log(converted);
+}
+upperCaseLetterConverter(prompt("Enter the statement: "));
+
+
+
+
+
+//Q12
+
+function longestWordChecker(sentence) {
+    var exString = sentence.split(" ");
+    var longestWord = exString[0];
+
+    for (let i = 1; i < exString.length; i++) {
+        if (exString[i].length > longestWord.length) {
+            longestWord = exString[i];
+        }
+    }
+
+    console.log("The longest word is", longestWord);
+}
+
+longestWordChecker(prompt("Enter the sentence: "));
+
+
+
+
+
+//Q14
+
+var radius = +prompt("Enter the radius: ");
+var pie = 3.142;
+function calcCircumference(radius) {
+    var circumference = 2 * pie * radius;
+    return circumference;
+}
+
+function calcArea(radius) {
+    var area = pie * (radius * radius);
+    return area;
+}
+var calulatedCircumference = calcCircumference(radius);
+var calculatedArea = calcArea(radius);
+console.log(`the Circumference is ${calulatedCircumference}`);
+console.log(`the Area is ${calculatedArea}`);
+
+
+
+
+
+//------------------------ch 38 - 42------------------------
+
+//Q1
+
+function power(base, exponent) {
+    var powerCalc = Math.pow(base, exponent);
+    console.log(powerCalc);
+}
+power(+prompt("write the base: "), +prompt("write the exponent: "));
+
+
+
+
+
+//Q2
+
+function leapYear(year) {
+    if (year % 4 == 0) {
+        console.log("The year is a leap year.");
+    } else {
+        console.log("It is not a leap year.");
+    }
+
+}
+leapYear(+prompt("Enter the year: "));
+
+
+
+
+//Q3
+
+var a = +prompt("Enter a length of side 1: ");
+var b = +prompt("Enter a length of side 2: ");
+var c = +prompt("Enter a length of side 3: ");
+function semiperimeter(a,b,c) {
+    return (a + b + c) / 2;
+}
+function areaOfTriangle(a,b,c){
+    var semiPerimeter = semiperimeter(a,b,c);
+    var areaOfTriangle = Math.sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c)).toFixed(2);
+    return areaOfTriangle;  
+}
+var result = areaOfTriangle(a,b,c);
+console.log("Area of triangle is",result);
+
+
+
+
